08f9a078d3702396d78c498e81bdee727eb8edd6
Björn-Lukas Böhnert <bboehnert@web.de>
2021-08-17 17:11:03 +0200

    Transitionen zwischen zwei Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-String _text = 'not changed';
-bool _pressed = true;
-const double _margin = 100;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() {
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        margin: const EdgeInsets.all(_margin),
-        child: ElevatedButton(
-          onPressed: _buttonPressed,
-          child: Center(
-            child: Text(_text),
-          ),
-        ),
+      home: Scaffold(
+        body: MainMenu(),
       ),
     );
   }
+}
 
-  void _buttonPressed() {
-    setState(() {
-      if (_pressed) {
-        _text = 'changed';
-      } else {
-        _text = 'not changed';
-      }
-      _pressed = !_pressed;
-    });
+class MainMenu extends StatelessWidget {
+  late BuildContext _context;
+
+  @override
+  Widget build(BuildContext context) {
+    _context = context;
+    // Column als Widget für die vertikale Anordnung der Container
+    // Container, um die Größe zu berechnen
+    // Fitted-Box um den Text auf die Größe des Containers zu erhöhen
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          child: FittedBox(child: Text('Atari Breakout')),
+        ),
+        Expanded(
+            child: Container(
+          child: FittedBox(
+              child: ElevatedButton(
+                  onPressed: () => _startGame(context, Game()),
+                  child: Text('Play Game'))),
+        ))
+      ],
+    );
   }
+}
+
+class Game extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          child: FittedBox(child: Text('Atari Breakout')),
+        ),
+        Expanded(
+            child: Container(
+          child: FittedBox(
+              child: ElevatedButton(
+                  onPressed: () => _startGame(context, MainMenu()),
+                  child: Text('Go back'))),
+        ))
+      ],
+    );
+  }
+}
+void _startGame(BuildContext context, StatelessWidget app) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => Scaffold(body: app)));
 }
